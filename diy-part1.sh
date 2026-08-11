be6ca1fa417eeb88fdabe076e33c00e839db0aa7
xiaochen66666 <150781778+xiaochen66666@users.noreply.github.com>
2026-08-11 12:23:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,10 +18,10 @@ git clone https://github.com/JE668/k3screenctrl_build.git package/k3screenctrl/
 echo '=========Replace k3screen drive plug OK!========='
 
 # 无线驱动版本选择 (1.'asus_dhd24' 2.'ac88u_20' 3.'69027')
-firmware='69027'
-echo '替换无线驱动'
-wget -nv https://github.com/JE668/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/firmware/brcmfmac4366c0-firmware-k3/files/brcmfmac4366c-pcie.bin
-echo '=========Replace k3 wireless firmware OK!========='
+# firmware='69027'
+# echo '替换无线驱动'
+# wget -nv https://github.com/JE668/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/firmware/brcmfmac4366c0-firmware-k3/files/brcmfmac4366c-pcie.bin
+# echo '=========Replace k3 wireless firmware OK!========='
 
-#Compile K3 Only
-sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
+# Compile K3 Only
+# sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
